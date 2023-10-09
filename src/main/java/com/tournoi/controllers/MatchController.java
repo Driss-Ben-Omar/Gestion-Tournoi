e0a354ee3fbcf0f01d0c9c9986765026ae7fc941
Driss Benomar <benomard999@gmail.com>
2023-10-09 18:03:59 +0100

    finish the player part and solve some problems in the team controller and view

--- src/main/java/com/tournoi/controllers/MatchController.java
+++ src/main/java/com/tournoi/controllers/MatchController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,51 +17,81 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tournoi.entities.Arbitre;
+import com.tournoi.entities.Equipe;
+import com.tournoi.entities.Joueur;
 import com.tournoi.entities.Match;
+import com.tournoi.entities.Stade;
+import com.tournoi.services.ArbitreService;
+import com.tournoi.services.EquipeService;
 import com.tournoi.services.MatchService;
+import com.tournoi.services.StadeService;
 
 @Controller
 public class MatchController {
-	
+
 	@Autowired
 	MatchService matchService;
 	
+	@Autowired
+	EquipeService equipeService;
+	
+	@Autowired
+	StadeService stadeService;
+	
+	@Autowired
+	ArbitreService arbitreService;
+
 	@GetMapping("/matchs")
-	public String getAll(Model model){
-		List<Match> matchs= matchService.getAll();
+	public String getAll(Model model) {
+		List<Match> matchs = matchService.getAll();
 		model.addAttribute("matchs", matchs);
 		return "match";
 	}
 	
-	@GetMapping("/match/{id}")
-	public @ResponseBody Match getById(@PathVariable Integer id) {
-		return matchService.getById(id);
+	@GetMapping("/formMatch")
+	public String formMatch(Model model) {
+		List<Equipe> equipes=equipeService.getAll();
+		List<Stade> stades=stadeService.getAll();
+		List<Arbitre> arbitres=arbitreService.getAll();
+		model.addAttribute("equipe", new Equipe());
+		model.addAttribute("equipes", equipes);
+		model.addAttribute("stades", stades);
+		model.addAttribute("arbitres", arbitres);
+		return "formMatch";
 	}
 	
-	@GetMapping("/match/date/{dateMatch}")
-	public @ResponseBody List<Match> getByDateMatch(@PathVariable("dateMatch") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateMatch) {
-	    return matchService.getByDateMatch(dateMatch);
-	}
-	
-	@PostMapping("/match")
-	public @ResponseBody Match save(@RequestBody Match match) {
-		return matchService.save(match);
-	}
-	
-	@PutMapping("/match")
-	public @ResponseBody Match update(@RequestBody Match match) {
+	@GetMapping("/match/edit")
+	public String editMatch(Model model,Integer id) {
 		
-		return matchService.update(match);
-	}
-	
-	@DeleteMapping("/match/{id}")
-	public @ResponseBody void deleteById(@PathVariable Integer id) {
-		matchService.deleteById(id);
+		Match match=matchService.getById(id);
+		model.addAttribute("match", match);
+		
+		List<Equipe> equipes=equipeService.getAll();
+		List<Stade> stades=stadeService.getAll();
+		List<Arbitre> arbitres=arbitreService.getAll();
+		model.addAttribute("equipes", equipes);
+		model.addAttribute("stades", stades);
+		model.addAttribute("arbitres", arbitres);
+		
+		return "editMatch";
 	}
 
-	@DeleteMapping("/match")
-	public @ResponseBody void delete(@RequestBody Match match) {
-		matchService.delete(match);
+	@PostMapping("/match/save")
+	public String saveMatchWithEquipes(Match match, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "formMatch";
+		}
+		matchService.save(match);
+
+		return "redirect:/matchs";
+	}
+	
+	@GetMapping("/match/delete/{id}")
+	public String deleteMatch(@PathVariable Integer id) {
+
+		matchService.deleteById(id);
+		return "redirect:/matchs";
 	}
 	
 }
